85d06e8d778a09193009ba91e5d7896964246565
nome-do-seu-usuario-de-login-do-github <email-que-usou-pra-criar-a-conta-do-github>
2018-06-05 14:41:09 -0300

    comentarios kwic

--- Tecnicas_de_prog_2/kwic/checkbox.py
+++ Tecnicas_de_prog_2/kwic/checkbox.py
@@ -2,14 +2,20 @@ from tkinter import *
 master = Tk()
 
 def var_states():
+	#Define variaveis dos botões
 	print("English: %d,\nPortuguese %d" % (var1.get(), var2.get()))
-
+#Define texto na primeira row com o seguinte texto
 Label(master, text="What language: ").grid(row=0, sticky=W)
+#Cria a variavel var1
 var1 = IntVar()
+#Cria o botão de checkbox com a opção de escolha ingles.
 Checkbutton(master, text="English", variable=var1).grid(row=1, sticky=W)
+#Cria a variavel var2
 var2 = IntVar()
+#Cria o botão de checkbox com a opção de escolha portugues
 Checkbutton(master, text="Portuguese", variable=var2).grid(row=2, sticky=W)
+#Cria dois botões, o primeiro sai do programa. O Segundo realiza a ação de mostrar o valor de var1 e var2.
 Button(master, text='Quit', command=master.quit).grid(row=3, sticky=W, pady=4)
 Button(master, text='Show', command=var_states).grid(row=4, sticky=W, pady=4)
-
+#Continuar na execucao do programa enquanto master.quit não é chamado
 mainloop()
--- Tecnicas_de_prog_2/kwic/kwic.py
+++ Tecnicas_de_prog_2/kwic/kwic.py
@@ -1,24 +1,31 @@
+#Import para o modulo para remover caracteres especiais
 import re
 
-# -*- coding: utf-8  -*-
+#-*- coding: utf-8  -*-
 
-
+#Funcao que remove caracteres nao alfa numericos
 def stripNonAlphaNum(x):
     return re.sub(r'[^a-zA-Z: ]', '', x) # x is my string to sanitize
-    #re.compile(r'\W+', re.UNICODE).split(text)
 
+
+#Remove as stopwords que ja foram abertas na execucao do programa.
 def removeStopwords(wordlist, stopwords):
     return [w for w in wordlist if w not in stopwords]
 
+#abre o texto para executar o kwic e salva em uma lista com todas as palvras
 wordlist = open('test.txt', 'r').read().split()
+#abre as stopwords em portugues com o .pt e ingles com o .en
+#todo: linkar as diferentes opcoes com o programa checkbox.py
 stoplist = open('stopwords.pt', 'r').read().split()
-#print(wordlist)
+#coloca as palavras em caixa-baixa
 wordlist = map(lambda x:x.lower(), wordlist)
+#chama a funcao para remocao de caracteres alfa-numericos
 wordlist = map(stripNonAlphaNum, wordlist)
+#chama a funcao para a remocao de stopwords
 wordlist = removeStopwords(wordlist, stoplist)
-
+#Separa de 5 em 5 palavras de wordlist em uma nova lista
 ngrams = [wordlist[i:i+5] for i in range(len(wordlist)-4)]
-
+#organiza o kwic para mostrar as palavras mais frequentes e constroi um dicionario (chave = palavra, valor = frequencia)
 kwicdict = {}
 for n in ngrams:
     if n[2] not in kwicdict:
@@ -26,18 +33,13 @@ for n in ngrams:
     else:
         kwicdict[n[2]].append(n)
 
-#print kwicdict
-
-
-
-for key in sorted(kwicdict.iterkeys()): 
+#por ordem de frequencia, imprime da mais frequente para a menos as palavras do dicionario anteriormente construido.
+for key in sorted(kwicdict.iterkeys()):
     for val in kwicdict[key]:
-
+#deixa bonito
        outstring = ' '.join(val[:2]).rjust(20)
        outstring += ' '
        outstring += ' '.join(str(val[2]).center(len(n[2])+6))
        outstring +=' '
        outstring += ' '.join(val[3:])
        print outstring
-
-
